e4ac99ab84fdf468288640ac20db8510ce3e7581
MikhaelEmpi <d.mikhaelempi@gmail.com>
2023-03-24 15:35:39 +0700

    feat: blade template&basic directives

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,12 @@
-<h1><?php echo $heading; ?></h1>
-<?php foreach($listings as $listing): ?>
-    <h2><?php echo $listing['title']; ?></h2>
-    <p><?php echo $listing['description']; ?></p>
-<?php endforeach; ?>
+<h1> {{ $heading }} </h1>
+
+@unless (count($listings) == 0)
+
+@foreach($listings as $listing)
+<h2> {{ $listing['title'] }} </h2>
+<p> {{ $listing['description'] }} </p>
+@endforeach
+
+@else
+<p>No Listings Found</p>
+@endunless
