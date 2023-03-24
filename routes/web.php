7a65e03ddc11d20c5935e22c8c9e3bd2dfdec401
MikhaelEmpi <d.mikhaelempi@gmail.com>
2023-03-24 11:33:55 +0700

    feat: routing&responses, wildcard endpoints. route constraints, dd helper

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return response('<h1>Hello World</h1>', 200)
+        ->header('Content-Type', 'text/plain')
+        ->header('andi', 'kunyit');
+});
+
+Route::get('/posts/{id}', function($id) {
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
