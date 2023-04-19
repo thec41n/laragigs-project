68f296899f3e24d3efc12944701f3582f75605a7
MikhaelEmpi <d.mikhaelempi@gmail.com>
2023-04-19 15:31:44 +0700

    feat: listing controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 /*
@@ -13,20 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
  */
 // All Listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all(),
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single Listing
-Route::get('/listings/{listing}', function (Listing $listing) {
-    
-    return view('listing', [
-        'listing' => $listing
-    ]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 // Route::get('/hello', function() {
 //     return response('<h1>Hello World</h1>', 200)
