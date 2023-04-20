e5818b08b1812d4400268b26d365b987ced28ffb
MikhaelEmpi <d.mikhaelempi@gmail.com>
2023-04-20 15:36:24 +0700

    feat: old value in create form

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -14,7 +14,7 @@
         @csrf
         <div class="mb-6">
             <label for="company" class="inline-block text-lg mb-2">Company Name</label>
-            <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company" />
+            <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company" value="{{old('company')}}"/>
 
             @error('company')
             <p class="text-red-500 text-xs mt-1"> {{$message}} </p>
@@ -24,7 +24,7 @@
         <div class="mb-6">
             <label for="title" class="inline-block text-lg mb-2">Job Title</label>
             <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
-                placeholder="Example: Senior Laravel Developer" />
+                placeholder="Example: Senior Laravel Developer" value="{{old('title')}}"/>
 
             @error('title')
             <p class="text-red-500 text-xs mt-1"> {{$message}} </p>
@@ -34,7 +34,7 @@
         <div class="mb-6">
             <label for="location" class="inline-block text-lg mb-2">Job Location</label>
             <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
-                placeholder="Example: Remote, Boston MA, etc" />
+                placeholder="Example: Remote, Boston MA, etc" value="{{old('location')}}"/>
 
             @error('location')
             <p class="text-red-500 text-xs mt-1"> {{$message}} </p>
@@ -44,7 +44,7 @@
 
         <div class="mb-6">
             <label for="email" class="inline-block text-lg mb-2">Contact Email</label>
-            <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" />
+            <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{old('email')}}"/>
 
             @error('email')
             <p class="text-red-500 text-xs mt-1"> {{$message}} </p>
@@ -55,7 +55,7 @@
             <label for="website" class="inline-block text-lg mb-2">
                 Website/Application URL
             </label>
-            <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website" />
+            <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website" value="{{old('website')}}"/>
 
             @error('website')
             <p class="text-red-500 text-xs mt-1"> {{$message}} </p>
@@ -67,7 +67,7 @@
                 Tags (Comma Separated)
             </label>
             <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
-                placeholder="Example: Laravel, Backend, Postgres, etc" />
+                placeholder="Example: Laravel, Backend, Postgres, etc" value="{{old('tags')}}"/>
 
             @error('tags')
             <p class="text-red-500 text-xs mt-1"> {{$message}} </p>
@@ -86,8 +86,7 @@
                 Job Description
             </label>
             <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10"
-                placeholder="Include tasks, requirements, salary, etc"></textarea>
-
+                placeholder="Include tasks, requirements, salary, etc">{{old('description')}}</textarea>
             @error('description')
             <p class="text-red-500 text-xs mt-1"> {{$message}} </p>
             @enderror
