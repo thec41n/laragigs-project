bc6792ad34e56780de7bb5b4863a6c41877194b2
MikhaelEmpi <d.mikhaelempi@gmail.com>
2023-04-20 14:13:55 +0700

    feat: create listing view&form

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,9 @@ use Illuminate\Support\Facades\Route;
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
+// Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
