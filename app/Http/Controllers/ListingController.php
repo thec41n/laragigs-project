c33b3a91c9edaaf9c615c644127a81e2db2dc29c
MikhaelEmpi <d.mikhaelempi@gmail.com>
2023-04-22 11:29:01 +0700

    feat: delete listing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -82,4 +82,10 @@ class ListingController extends Controller
 
         return back()->with('message', 'Listing updated successfully!');
     }
+
+    public function destroy(Listing $listing)
+    {
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing deleted successfully');
+    }
 }
